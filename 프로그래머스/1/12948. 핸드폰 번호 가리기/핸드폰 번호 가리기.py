d2da656715d769e4a9a2962c9b68f087b56111c4
Yerin <84303627+yerin08@users.noreply.github.com>
2025-12-29 15:52:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,11 +1,4 @@
 def solution(phone_number):
-    answer = ''
-    pn_list = list(phone_number)
-    
-    for i in range(len(pn_list)-4):
-        pn_list[i] = '*'
-    
-    for n in pn_list:
-        answer += n
+    answer = '*'*(len(phone_number)-4) + phone_number[-4:]
         
     return answer

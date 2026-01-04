f9085bb8864ff28f46c728792128fb38d8ddcd55
Yerin <84303627+yerin08@users.noreply.github.com>
2026-01-04 19:52:49 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -2,16 +2,14 @@ def solution(s):
     answer_list = []
     
     for word in s.split(' '):
-        index = 0
         answer = ''
         for c in word:
-            if index == 0:
+            if answer == '':
                 answer += c.upper()
             elif c.upper():
                 answer += c.lower()
             else:
                 answer += c
-            index += 1
         answer_list.append(answer)
         
     return ' '.join(answer_list)

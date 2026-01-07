aac03ea87d6ec55c395e72deb548b4826132296a
Yerin <84303627+yerin08@users.noreply.github.com>
2026-01-07 16:28:36 +0900

    [level 1] Title: 삼총사, Time: 0.53 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,8 +1,9 @@
 def solution(number):
     answer = 0
-    for i in range(len(number)-2):
-        for j in range(i+1, len(number)-1):
-            for k in range(j+1, len(number)):
+    l = len(number)
+    for i in range(l-2):
+        for j in range(i+1, l-1):
+            for k in range(j+1, l):
                 print(number[i], number[j], number[k])
                 if number[i] + number[j] + number[k] == 0:
                     answer += 1

e489c31ebe3af236286480e7da0148a9ae5a5d27
Yerin <84303627+yerin08@users.noreply.github.com>
2026-01-08 12:42:40 +0900

    [level 2] Title: 숫자의 표현, Time: 4.03 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,12 @@
 def solution(n):
-    answer = 1
+    answer = 0
     
-    for i in range(1, int(n / 2)+1):
+    for i in range(1, n+1):
         sum_n = 0
-        sum_n += i
-        for j in range(i+1, int(n / 2)+2):
-            sum_n += j
-            if sum_n == n:
-                answer += 1
-                break
-            elif sum_n > n:
-                break
+        while sum_n < n:
+            sum_n += i
+            i += 1
+        if sum_n == n:
+            answer += 1
         
     return answer

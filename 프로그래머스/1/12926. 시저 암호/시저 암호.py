7c7d2137d14692e1cd9b1e719ae8ac516de8a3b5
Yerin <84303627+yerin08@users.noreply.github.com>
2026-01-18 18:57:35 +0900

    [level 1] Title: 시저 암호, Time: 2.37 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,20 +1,10 @@
 def solution(s, n):
-    answer = ''
+    answer = list(s)
     
-    for c in s:
-        if c == ' ':
-            answer += " "
-        elif c.isupper():
-            if ord(c) - ord("A") + n >= 26:
-                asc = ord(c) - ord("A") + n - 26
-                answer += chr(ord("A") + asc)
-            else:
-                answer += chr(ord(c) + n)
-        elif c.islower():
-            if ord(c) - ord("a") + n >= 26:
-                asc = ord(c) - ord("a") + n - 26
-                answer += chr(ord("a") + asc)
-            else:
-                answer += chr(ord(c) + n)
+    for i in range(len(answer)):
+        if answer[i].isupper():
+            answer[i] = chr(((ord(answer[i]) - ord("A") + n) % 26) + ord("A"))
+        elif answer[i].islower():
+            answer[i] = chr(((ord(answer[i]) - ord("a") + n) % 26) + ord("a"))
             
-    return answer
+    return "".join(answer)

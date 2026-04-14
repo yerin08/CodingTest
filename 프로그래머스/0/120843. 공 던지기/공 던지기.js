c3c9893c405f39b83b685fca915674ac9dc07889
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-14 09:36:56 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,11 +1,5 @@
 function solution(numbers, k) {
     let turn = 2 * (k - 1);
     
-    if (turn <= numbers.length - 1) {
-        return numbers[turn];
-    } else {
-        return numbers[turn % numbers.length];
-        
-    }
-
+    return numbers[turn % numbers.length];
 }

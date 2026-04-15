45ccaf87e5b4ce44c666a6dc0719f95c794856ae
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-16 08:35:58 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -2,7 +2,7 @@ function solution(my_string) {
     const answer = [];
     
     for (let c of my_string) {
-        if (parseInt(c) >= 0) answer.push(parseInt(c));
+        if (!isNaN(c)) answer.push(parseInt(c));
     }
 
     answer.sort((a, b) => a - b);

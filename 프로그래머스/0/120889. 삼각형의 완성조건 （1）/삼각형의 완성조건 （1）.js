c11d6794518ab7ef752e483798db3f4fbe444306
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-17 13:48:50 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,8 @@
+function solution(sides) {
+    sides.sort((a, b) => a - b);
+    const max = sides.pop();
+    const others = sides.reduce((a, b) => a + b, 0);
+    
+    if (max < others) return 1;
+    else return 2;
+}

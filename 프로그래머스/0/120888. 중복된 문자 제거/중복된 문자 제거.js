b2be821c3ebb23ed7210a92533031ab2f6af2987
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-17 13:40:29 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,10 +1,3 @@
 function solution(my_string) {
-    const answer = [];
-    
-    for (let c of my_string) {
-        if (answer.includes(c)) continue;
-        answer.push(c);
-    }
-    
-    return answer.join('');
+    return ([...new Set(my_string)].join(''));
 }

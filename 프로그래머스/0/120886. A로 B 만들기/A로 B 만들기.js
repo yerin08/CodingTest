8f0b534379a39551c385b1c60241266fe9a05a46
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-27 08:11:27 +0900

    [level 0] Title: A로 B 만들기, Time: 4.74 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,11 @@
+function solution(before, after) {
+    const after_list = [...after];
+    
+    for (let c of before) {
+        let index = after_list.indexOf(c);
+        if (index !== -1) after_list.splice(index, 1);
+    }
+    
+    console.log(after_list);
+    return after_list.length === 0 ? 1 : 0;
+}

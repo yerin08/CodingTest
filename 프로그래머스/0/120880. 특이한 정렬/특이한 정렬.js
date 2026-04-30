4dbc3764c42b76e8bcf922f440141675325ba896
Yerin <84303627+yerin08@users.noreply.github.com>
2026-04-30 10:21:03 +0900

    [level 0] Title: 특이한 정렬, Time: 0.76 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -4,18 +4,14 @@ function solution(numlist, n) {
     const answer = [];
     
     numlist.forEach(i => num_dict[i] = Math.abs(n - i));
-    console.log(num_dict);
+
     const value_list = [...new Set([...Object.values(num_dict).sort((a, b) => a - b)])];
-    console.log(value_list);
     const key_list = Object.keys(num_dict);
-    console.log(key_list);
     
     while (value_list.length !== 0) {
         min_value = value_list.shift();
-        console.log(min_value);
         let add_nums = key_list.filter(key => num_dict[key] === min_value);
         add_nums = add_nums.map(Number).sort((a, b) => b - a);
-        console.log(add_nums);
         answer.push(...add_nums);
     } 
     
